3d0148ebf1c7b83935ebefefad92241f55fecc98
stefanolomo <stefanolomo@gmail.com>
2025-03-20 15:47:41 -0300

    Practica 2

---
+++ Practica 2/multiplosDe3/MultiplosDe3.pas
@@ -0,0 +1,29 @@
+
+Program Multiplos;
+
+Var 
+    digito, num:   integer;
+
+Begin
+    num := 0;
+    writeln('Ingrese el numero a analizar: ');
+    readln(num);
+    while (num > 0) do
+        Begin
+            digito := num mod 10;
+            // Extraemos el último dígito
+
+            if (digito mod 3 = 0) then
+                writeln(digito, ' es multiplo de 3')
+                // Si es múltiplo de 3, lo imprimimos
+                
+            else if (digito mod 3 <> 0) then
+                writeln(digito, ' no es multiplo de 3');
+            
+            num := num div 10;
+            // writeln(num)
+            // Eliminamos el último dígito
+        End;
+
+
+End.
