95817bfb3c1564c2233da8d6f094289974f35966
stefanolomo <stefanolomo@gmail.com>
2025-03-25 11:24:19 -0300

    Inciso C

--- Practica 2/CaracteresConPunto/Caracteres.pas
+++ Practica 2/CaracteresConPunto/Caracteres.pas
@@ -2,17 +2,30 @@
 Program Caracteres;
 
 Var 
-    letra:   char;
+    letra, ant:   char;
     contador:   integer;
 
 Begin
+    //$ Inicializa la variable del contador...
     contador := 0;
+
+    //> Lee por teclado la secuencia de caracteres...
     writeln('Ingrese la secuencia de caracteres');
+    read(letra);
 
-    Repeat
-        read(letra);
-        if (letra = 'E') then contador = contador + 1;
-    Until (letra = '.');
-    writeln('La cantidad de caracteres leidos es de: ', contador - 1);
+    //? Mientras la primera letra que leyó no sea el punto...
+    While (letra <> '.') Do
+        Begin
+            //$ Iguala la variable de 'Anterior' a la letra leida...
+            ant := letra;
+            //> Lee de vuelta la siguiente letra
+            read(letra);
 
+            //? Si la letra anterior era 'f' y la actual es 'e' suma al contador 1...
+            If (ant = 'f') And (letra = 'e') Then
+                contador := contador + 1;
+        End;
+
+    //> Informa la cantidad de secuencias 'fe' leidas...
+    writeln('La cantidad de fe encontrados es: ', contador);
 End.
