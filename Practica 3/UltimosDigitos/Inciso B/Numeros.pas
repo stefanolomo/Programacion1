506b3398131c74869ea060e8f8fc26364d5f3f76
stefanolomo <stefanolomo@gmail.com>
2025-04-09 21:26:19 -0300

    Inciso B

--- Practica 3/UltimosDigitos/Inciso B/Numeros.pas
+++ Practica 3/UltimosDigitos/Inciso B/Numeros.pas
@@ -1,7 +1,41 @@
-program NumerosPorTeclado;
+
+Program NumerosPorTeclado;
+
+Var 
+    num:   longint;
 
 // B. Escriba un programa que lea números enteros por teclado hasta que llegue el número 0. Utilice el módulo implementado en a) para imprimir los últimos 3 y 5 dígitos de cada número ingresado
 
-begin
-    
-end.
+Procedure ultimosDigitos(num: longint; n: integer);
+
+Var 
+    digito, i:   integer;
+
+Begin
+    For i := 1 To n Do
+        Begin
+            digito := num Mod 10;
+            write(digito);
+            num := num Div 10;
+        End;
+    writeln('');
+End;
+
+Begin
+    num := 1;
+
+    While (num <> 0) Do
+        Begin
+            writeln('Ingrese el nro: ');
+            readln(num);
+
+            If (num <> 0) Then
+                Begin
+
+                    ultimosDigitos(num, 5);
+                    ultimosDigitos(num, 3);
+
+                End;
+
+        End;
+End.
