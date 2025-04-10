e3d439608290a90406242f16336201e7fd3f9692
stefanolomo <stefanolomo@gmail.com>
2025-04-09 21:08:54 -0300

    Format

---
+++ Practica 3/UltimosDigitos/Inciso B/Numeros.pas
@@ -0,0 +1,7 @@
+program NumerosPorTeclado;
+
+// B. Escriba un programa que lea números enteros por teclado hasta que llegue el número 0. Utilice el módulo implementado en a) para imprimir los últimos 3 y 5 dígitos de cada número ingresado
+
+begin
+    
+end.
