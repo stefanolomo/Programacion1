859d933477f4f0fe68d9080b29b35f50471f9b9e
stefanolomo <stefanolomo@gmail.com>
2025-05-09 18:40:08 -0300

    Descomponer codigo del robot procedure

--- EjercicioIntegrador/integrador.pas
+++ EjercicioIntegrador/integrador.pas
@@ -315,10 +315,35 @@ Begin
     CheckParteC := Esvalido And encontreAB;
 End;
 
-procedure DescomponerVectorCodigo(CodigoRobot: VectorCodigo; var secuenciaA: VectorParteA; var secuenciaB: VectorParteB; var secuenciaC: VectorParteC);
-begin
-    
-end;
+Procedure DescomponerVectorCodigo(
+    codigo: VectorCodigo;
+    Var A: VectorParteA;
+    Var B: VectorParteB;
+    Var C: VectorParteC
+    );
+
+Var
+    i: integer;
+
+Begin
+    // Parte A: Primeros 6 caracteres
+    For i := 1 to 6 do
+        A.V[i] := codigo.V[i];
+    A.DimL := 6;
+    A.DimF := 6;
+
+    // Parte B: Siguientes 5 caracteres
+    For i := 1 to 5 do
+        B.V[i] := codigo.V[i + 6];
+    B.DimL := 5;
+    B.DimF := 5;
+
+    // Parte C: Ultimos 7 caracteres
+    For i := 1 to 7 do
+        C.V[i] := codigo.V[i + 11];
+    C.DimL := 7;
+    C.DimF := 7;
+End;
 
 Function CodigoValido(CodigoRobot: VectorCodigo):   boolean;
 
@@ -332,7 +357,7 @@ Begin
     DescomponerVectorCodigo(CodigoRobot, secuenciaA, secuenciaB, secuenciaC);
 
     // Pasamos cada parte a cada check y ese sera el valor de la funcion
-    CodigoValido := CheckParteA(secuenciaA) and CheckParteB(secuenciaB, secuenciaA) and CheckParteC(secuenciaC, secuenciaA);  
+    CodigoValido := CheckParteA(secuenciaA) and CheckParteB(secuenciaB, secuenciaA) and CheckParteC(secuenciaC, secuenciaA);
 End;
 
 Begin
