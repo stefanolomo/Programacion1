dc2e69425d8edc34d8d7d89add7293383df78a40
stefanolomo <stefanolomo@gmail.com>
2025-06-27 08:41:11 -0300

    consigna ejercicio 4

---
+++ Practica 9/Ejercicio 4/program.pas
@@ -0,0 +1,7 @@
+program Ejercicio4;
+
+// Dada una lista de aviones (marca, modelo y cantidad de asientos), definir la estructura que permita almacenarlos y escribir un programa que implemente:
+
+// a) Un módulo que reciba una lista de aviones y un avión y lo agregue a la lista. Se sabe que la lista está ordenada por marca en forma descendente y se pide agregar el avión a la lista de forma que se mantenga el orden.
+
+// b) Un módulo que reciba una lista de aviones y un avión, y elimine el elemento de la lista que coincida totalmente con el avión recibido. Además, debe retornar si la eliminación se realizó o no.
