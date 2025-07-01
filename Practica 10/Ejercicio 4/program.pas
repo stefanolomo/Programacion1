04049a394825474b6e893773631370274e3d2392
stefanolomo <stefanolomo@gmail.com>
2025-06-30 22:36:10 -0300

    Ejercicio 4 tipos

---
+++ Practica 10/Ejercicio 4/program.pas
@@ -0,0 +1,18 @@
+Program Ejercicio4;
+
+{ Una empresa de la ciudad de La Plata, que realiza entregas de paquetes dentro del casco urbano, está interesada en procesar la información de sus paquetes. De cada paquete se conoce la fecha de envío, si pudo ser entregado al destinatario y la dirección que está compuesta por los campos: calle (de 1 a 122), número, piso y departamento. 
+
+Se dispone de una lista con los envíos del año pasado. Se requiere procesar la lista recorriéndola una sola vez para:
+
+a. Informar la cantidad de envíos realizados para cada calle.
+
+b. Informar el nombre del mes con mayor cantidad de paquetes enviados.
+
+c. Eliminar de la lista de envíos, aquellos que no fueron entregados al destinatario.
+
+d. Generar 10 nuevas listas con los envíos de las calles 11 a la 20. Cada lista debe estar ordenada por el número de la dirección. }
+
+type
+    paquete = record
+    
+    end;
