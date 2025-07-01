bad25eba647dbd9598124098d0ed4f0dc4b54eb6
stefanolomo <stefanolomo@gmail.com>
2025-07-01 19:46:17 -0300

    consigna ejercicio 5

---
+++ Practica 10/Ejercicio 5/program.pas
@@ -0,0 +1,15 @@
+Program Ejercicio5;
+
+{ Se desea modelar un nuevo juego que consiste en un tablero de 15 filas por 15 columnas y que se juega de a 2 jugadores. Cada casillero del tablero contiene un número del 0 al 10 y un valor que indica si la celda fue utilizada a lo largo de la partida.
+
+La partida consiste de 10 rondas donde en cada una de las rondas los 2 jugadores eligen una celda del tablero. Las celdas elegidas en cada ronda se analizan y obtiene un punto el jugador que eligió la celda que contiene el mayor número. En caso de que ambos jugadores hayan, en una ronda, elegido una celda que contiene el mismo número ninguno suma puntos. 
+
+En caso de que algún jugador seleccione una celda que ya haya sido utilizada a lo largo de la partida, dicho jugador pierde la ronda y se le suma un punto a su oponente. En caso de que en una ronda ambos jugadores seleccionen celdas que ya hayan sido utilizadas a lo largo de la partida, ninguno suma puntos. Se pide:
+
+a. Implemente un módulo que inicialice el tablero de juego. El módulo recibe el tablero y una lista con los valores que va a tener cada celda del tablero. Cada elemento de la lista almacena: fila, columna y número para la celda (0 a 10). Además, cada celda se debe marcar como no utilizada. En caso de que el valor para una celda no venga en la lista, dicha celda se inicializara con el valor 0 (cero).
+
+b. Implemente un módulo que reciba el tablero y simule el juego. Los valores que cada jugador va eligiendo son leídos de teclado. Al finalizar el juego, informar los puntos obtenidos por cada jugador y cuál es el ganador }
+
+begin
+    
+end;
