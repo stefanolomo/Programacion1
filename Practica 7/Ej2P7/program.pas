5f2dcc068f55a55409b1e5096eeaf65073b1379a
stefanolomo <stefanolomo@gmail.com>
2025-07-01 19:47:20 -0300

    consigna ej2p7

---
+++ Practica 7/Ej2P7/program.pas
@@ -0,0 +1,3 @@
+{ *Realizar un módulo que reciba un vector A y dos valores I, J. El módulo debe devolver el vector A del
+cual se suprimen las componentes que se encuentran entre las posiciones I y J (inclusive las
+componentes de dichas posiciones) }
