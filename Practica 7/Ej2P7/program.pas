513fb3a5248a7f7eefbd0a61df3ecdfc458c1a89
stefanolomo <stefanolomo@gmail.com>
2025-07-03 22:11:56 -0300

    Insertar Ordenado doble enlaze pri ult

--- Practica 7/Ej2P7/program.pas
+++ Practica 7/Ej2P7/program.pas
@@ -1,3 +1,25 @@
-{ *Realizar un módulo que reciba un vector A y dos valores I, J. El módulo debe devolver el vector A del
-cual se suprimen las componentes que se encuentran entre las posiciones I y J (inclusive las
-componentes de dichas posiciones) }
+program Ej2P7;
+
+const
+    SUP = 10;
+    INF = 1;
+
+{ *Realizar un módulo que reciba un vector A y dos valores I, J. El módulo debe devolver el vector A del cual se suprimen las componentes que se encuentran entre las posiciones I y J (inclusive las componentes de dichas posiciones) }
+
+type
+    rango = INF..SUP;
+    Vector = Array[rango] of integer;
+
+procedure EliminarRango(var V: Vector; lsup: rango; linf: rango);
+
+var
+    i: integer;
+
+begin
+    for i := lsup downto linf do begin
+        V[i] := V[i+1];
+    end;
+end;
+
+begin
+end.
